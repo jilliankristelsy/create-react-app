9a2c100a000b03449c83cc28dc5de29117eec956
Jillian Kristel Sy <40276133+jilliankristelsy@users.noreply.github.com>
2018-06-25 08:35:25 -0700

    updates for surfair

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -3,23 +3,82 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+	
+	this.state = {
+	  apiKey: "wLgLNY6dMO7TiUUnk5vIeMsfRVFETtx7",
+	  symbols: [],
+	  selectedSymbol: "",
+	  quote: []
+	}
+	
+	this.getSymbols = this.getSymbols.bind(this);
+	this.getQuotes = this.getQuotes.bind(this);
+	this.handleChangeSymbol = this.handleChangeSymbol.bind(this);
+	this.handleRefresh = this.handleRefresh.bind(this);
+	this.handleSubmit = this.handleSubmit.bind(this);
+	
+  }
+  
+  componentDidMount() {
+    this.getSymbols();
+  }
+  
+  getSymbols = () => {
+	const url = `https://forex.1forge.com/1.0.3/symbols?api_key=${this.state.apiKey}`
+	fetch(url).then(results => {
+	  return results.json();
+	}).then(data => {
+  	  this.setState({
+	    symbols: data
+	  });
+	});
+  }
+  
+  getQuotes = selectedSymbol => {
+	const url = `https://forex.1forge.com/1.0.3/quotes?pairs=${selectedSymbol}&api_key=${this.state.apiKey}`;
+	fetch(url).then(results => {
+	  return results.json();
+	}).then(data => {
+  	  this.setState({
+	    quote: data[0]
+	  });
+	});
+  }
+  
+  handleChangeSymbol = (e) => {
+    this.setState({selectedSymbol: e.target.value});
+  }
+  
+  handleRefresh = () => {
+    this.handleSubmit();
+  }
+  
+  handleSubmit = () => {
+    const {selectedSymbol} = this.state;
+    this.getQuotes(selectedSymbol);
+  }
+  
   render() {
+    const {symbols, selectedSymbol, quote} = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+		  <select value={selectedSymbol} onChange={this.handleChangeSymbol}>
+		    <option value=""></option>
+			{
+			  symbols.map((sym) => <option key={sym} value={sym}>{sym}</option>)
+			}
+		  </select>
+		<button onClick={this.handleSubmit}>Submit</button>
+		<button onClick={this.handleRefresh}>Refresh</button>
+		
+		{quote.symbol !== undefined && <div>
+	      <h1>Symbol: {quote.symbol}</h1>
+	      <p>Bid: {quote.bid}</p>
+	      <p>Ask: {quote.ask}</p>
+	      <p>Price: {quote.price}</p>
+	    </div>}
       </div>
     );
   }
